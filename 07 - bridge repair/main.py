36c11b54b1ac98218e75713aff97bb65966c1abc
Karston Kuciemba <kuciembakarston@gmail.com>
2024-12-06 23:24:26 -0600

    add pipe operator for part 2 - complete!

--- 07 - bridge repair/main.py
+++ 07 - bridge repair/main.py
@@ -37,6 +37,26 @@ The engineers just need the total calibration result, which is the sum of the te
 be true. In the above example, the sum of the test values for the three equations listed above is 3749.
 
 Determine which equations could possibly be true. What is their total calibration result?
+
+
+PART 2
+
+The engineers seem concerned; the total calibration result you gave them is nowhere close to being within safety tolerances.
+Just then, you spot your mistake: some well-hidden elephants are holding a third type of operator.
+
+The concatenation operator (||) combines the digits from its left and right inputs into a single number. For example, 12 || 345
+would become 12345. All operators are still evaluated left-to-right.
+
+Now, apart from the three equations that could be made true using only addition and multiplication, the above example has three
+more equations that can be made true by inserting operators:
+
+156: 15 6 can be made true through a single concatenation: 15 || 6 = 156.
+7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
+192: 17 8 14 can be made true using 17 || 8 + 14.
+Adding up all six test values (the three that could be made before using only + and * plus the new three that can now be made by
+also using ||) produces the new total calibration result of 11387.
+
+Using your new knowledge of elephant hiding spots, determine which equations could possibly be true. What is their total calibration result?
 """
 def evaluate_expression(numbers, operators):
     """Evaluate an expression with given numbers and operators
@@ -47,9 +67,11 @@ def evaluate_expression(numbers, operators):
     # apply operators left to right
     for i, op in enumerate(operators):
         if op == '+':
-            result += numbers[i+1]
-        else:
-            result *= numbers[i+1]
+            result += numbers[i + 1]
+        elif op == '*':
+            result *= numbers[i + 1]
+        elif op == '||':
+            result = int(str(result) + str(numbers[i + 1]))
     
     return result
 
@@ -61,7 +83,7 @@ def can_solve_equation(line):
     numbers = list(map(int, number_str.split()))
     
     # try all possible operator combinations (just + and *)
-    operators = ['+', '*']
+    operators = ['+', '*', '||']
     
     # generate all possible operator configurations
     def generate_operator_configs(length):
